f0eeecf6bee27e8ca85fae48f5aaa7b4c4db1692
Marcelo Vieira <marcelodefy15@gmail.com>
2023-02-07 19:13:46 -0300

    Feat - Requisito 3
    Construída rota para cadastro de usuários
    
    Construído middleware para gerar token jwt

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -10,7 +10,9 @@ const { JWT_SECRET } = process.env;
 const generateJwt = (req: Request, res: Response, _next: NextFunction) => {
   const data = req.body;
   delete data.password;
-  res.status(201).json({ token: sign(data, JWT_SECRET as string) });
+  res.status(201).json(
+    { token: sign(data, JWT_SECRET as string) },
+  );
 };
 
 export default generateJwt;
